9d504c3170256840060431130d8456c920189642
Md.Minhajul Islam <31633249+differentcoder@users.noreply.github.com>
2022-05-19 16:11:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello From Doctor Server!')
+})
+
+app.listen(port, () => {
+  console.log(`Doctors App Listening on Port ${port}`)
+})
